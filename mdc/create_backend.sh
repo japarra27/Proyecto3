ac939f9b4b26b4609d2ec441fd69b2b87b1549ca
Jaime Andrés Parra Mora <j.parram@uniandes.edu.co>
2020-10-25 16:01:55 -0500

    GCE

--- mdc/create_backend.sh
+++ mdc/create_backend.sh
@@ -1,10 +1,32 @@
-#!/bin/bash
-sudo apt-get -y update && \
-sudo apt install -y python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx curl && \
-sudo gsutil cp "gs://dsc-projects-mdc/.bashrc" "prueba" && \
-git clone "https://github.com/japarra27/Proyecto3.git" ~/proyecto3 && \
-cd proyecto3/mdc/backend && \
-pip3 install -r requirements.txt && \
-python3 manage.py makemigrations && \
-python3 manage.py migrate && \
-python3 manage.py runserver 0:8080
+# [START getting_started_gce_startup_script]
+# Install Stackdriver logging agent
+sudo curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+sudo bash install-logging-agent.sh
+
+# Install or update needed software
+sudo apt-get -y update
+sudo apt install -yq python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx git supervisor
+sudo pip3 install --upgrade pip virtualenv
+
+# Account to own server process
+useradd -m -d /home/pythonapp pythonapp
+
+# Fetch source code
+export HOME=/root
+sudo git clone https://github.com/japarra27/Proyecto3.git /opt/app
+
+# Python environment setup
+sudo virtualenv -p python3 /opt/app/gce/env
+source /opt/app/gce/env/bin/activate
+/opt/app/gce/env/bin/pip install -r /opt/app/mdc/backend/requirements.txt
+
+# Set ownership to newly created account
+chown -R pythonapp:pythonapp /opt/app
+
+# Put supervisor configuration in proper place
+cp /opt/app/gce/python-app.conf /etc/supervisor/conf.d/python-app.conf
+
+# Start service via supervisorctl
+supervisorctl reread
+supervisorctl update
+# [END getting_started_gce_startup_script]
